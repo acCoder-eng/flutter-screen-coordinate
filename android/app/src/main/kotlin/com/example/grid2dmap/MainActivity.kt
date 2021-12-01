ec9f0b6cf6cca6d17513268f526edaae563eb2a5
acCoder-eng <citirahmett0@gmail.com>
2021-12-01 22:11:34 +0300

    gridmap
    flutter 2d gridmap

---
+++ android/app/src/main/kotlin/com/example/grid2dmap/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.grid2dmap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: Scaffold(
+          body: gridexam(),
+        ));
+  }
+}
+
+class gridexam extends StatelessWidget {
+  int dx = 0;
+  int dy = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.black87, width: 2),
+            color: Colors.black38),
+        child: GestureDetector(
+          onPanUpdate: (detail) {
+            print("its Dragged");
+            print("global dx : " + detail.globalPosition.dx.toString());
+            print("global dy : " + detail.globalPosition.dy.toString());
+
+            RenderBox box = context.findRenderObject() as RenderBox;
+            Offset local = box.globalToLocal(detail.globalPosition);
+
+            print("local dx : " + local.dx.toString());
+            print("local dy : " + local.dy.toString());
+          },
+        ),
+      ),
+    );
+  }
+}
